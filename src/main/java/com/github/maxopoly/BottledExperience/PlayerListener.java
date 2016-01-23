6cb19b20e462c94c6fc429a779740afbb19f7625
Daniel Boston <programmerdan@gmail.com>
2016-01-23 12:12:47 -0500

    So totalExperience gives total xp gained since last death; its not appropriate here. Replaced with correct function that computes current experience since spigot doesnt appear to include one

--- src/main/java/com/github/maxopoly/BottledExperience/PlayerListener.java
+++ src/main/java/com/github/maxopoly/BottledExperience/PlayerListener.java
@@ -25,18 +25,19 @@ public class PlayerListener implements Listener {
 		Block b = e.getClickedBlock();
 		if (e.getAction() == Action.LEFT_CLICK_BLOCK && b != null && b.getType() == Material.ENCHANTMENT_TABLE) {
 			Player p = e.getPlayer();
+			int totalExperience = computeCurrentXP(p);
 			if (p.getItemInHand() != null && p.getItemInHand().getType() == Material.GLASS_BOTTLE &&
-					p.getTotalExperience() >= plugin.getXpPerBottle() ) {
+					totalExperience >= plugin.getXpPerBottle() ) {
 				ItemMap playerInv = new ItemMap(p.getInventory());
 				int bottles = playerInv.getAmount(new ItemStack( Material.GLASS_BOTTLE));
-				int xpavailable = p.getTotalExperience() / plugin.getXpPerBottle();
+				int xpavailable = totalExperience / plugin.getXpPerBottle();
 				int remove = Math.min(bottles, xpavailable);
 				log(p.getName() + "interacted with enchanting table, inventory has " + bottles + " bottles, " 
-						+ p.getTotalExperience() + " total experience, enough to fill " + remove + " bottles");
+						+ totalExperience + " total experience, enough to fill " + remove + " bottles");
 				if (remove > 0) {
-					int endXP = p.getTotalExperience() - (remove * plugin.getXpPerBottle());
+					int endXP = totalExperience - (remove * plugin.getXpPerBottle());
 					p.setLevel(0);
-					p.setTotalExperience(0);
+					p.setExp(0f);
 					log("Set xp for " + p.getName() + " to " + endXP);
 					p.giveExp(endXP);
 					ItemMap removeMap = new ItemMap();
@@ -63,6 +64,21 @@ public class PlayerListener implements Listener {
 		if (e.getExperience() != plugin.getXpPerBottle()) {
 			log("Xp control lost: " + e.getExperience());
 		}
+	}
+
+	/**
+	 * This is 1.8 mechanics only.
+	 */
+	private int computeCurrentXP(Player p) {
+		float cLevel = (float) p.getLevel();
+		float progress = p.getExp();
+		float a = 1f, b = 6f, c = 0f, x = 2f, y = 7f;
+		if (cLevel > 16 && cLevel <= 31) {
+			a = 2.5f; b = -40.5f; c = 360f; x = 5f; y = -38f;
+		} else if (cLevel >= 32) {
+			a = 4.5f; b = -162.5f; c = 2220f; x = 9f; y = -158f;
+		}
+		return (int) Math.floor(a * cLevel * cLevel + b * cLevel + c + progress * (x * cLevel + y));
 	}
 
 	public void log(String msg) {
